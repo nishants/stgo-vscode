ccb035fb8d8ac8a2d949fe7909629f923203be34
Himanshu Saini <himanshusaini2212@bitbucket.com>
2020-04-03 00:17:00 +0530

    label changes

--- ui/tabs-buttons/index.js
+++ ui/tabs-buttons/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import { TABS } from "../constants";
 
 export default ({ selectTab, selectedTab }) => {
+  const TAB_NAMES = {
+    overview: 'Overview',
+    cypressCi: 'Cypress CI',
+    screenshotDiffs: 'Screenshot Differences',
+    ciLogs: 'Branch Logs',
+  };
 
   const getTabButton = (key) => {
       let className1 = "tab";
@@ -9,8 +15,8 @@ export default ({ selectTab, selectedTab }) => {
       if(TABS[key]==selectedTab) {
         className1+= " tab--active";
       }
-      
-      return <button className={className1} onClick={() => selectTab(TABS[key])}>{TABS[key]}</button>
+
+      return <button className={className1} onClick={() => selectTab(TABS[key])}>{TAB_NAMES[key]}</button>
   }
 
   return (
