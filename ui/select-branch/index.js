2c3af0aaa4754dc0da2b62bab674acc0314e0375
deepsaurabh <deepglb@gmail.com>
2020-04-02 23:06:27 +0530

    populate current branch with branch list and show current branch as seleted

--- ui/select-branch/index.js
+++ ui/select-branch/index.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default (props) => (
   <div id='branch-selector'>
-    <select id="branch" onChange={props.selectBranch}>
+    <select id="branch" onChange={props.selectBranch} value={props.currentBranch}>
       {props.list.map(branch => {
-        return <option value={branch.id}>{branch.name}</option>
+        return <option value={branch.name}>{branch.name}</option>
       })}
     </select>
     <button type="button" class="refresh">Refresh</button>
