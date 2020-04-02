16ff1f25a9a556c61d56b41ca47b8a864b9b6ff2
Himanshu Saini <himanshusaini2212@bitbucket.com>
2020-04-02 17:31:25 +0530

    button styling

--- ui/cypress-ci/cypressCi.js
+++ ui/cypress-ci/cypressCi.js
@@ -1,32 +1,12 @@
 import React from "react";
 
 class CypressCi extends React.Component {
-    componentDidMount() {
-    //   this.messageListener = window.addEventListener('message', event => {
-    //     const message = event.data;
-    //     console.log("Received data from shell : ", message);
-    //     switch (message.messageId) {
-    //       case 'retried':
-    //         this.setMessage('Cant run more than one window. !');
-    //         break;
-    //     }
-    //   });
-    //   // TODO : Just to test, remove this
-    //   sendMessage({messageId: 'get-current-branch-info'});
-    //   sendMessage({messageId: 'get-pull-request', data: {branchName: "xyz-branch"}});
-    //   sendMessage({messageId: 'get-cypress-builds', data: {branchName: "xyz-branch"}});
-    }
-  
-    componentWillUnmount() {
-    //   window.removeEventListener(this.messageListener);
-    }
-  
     render() {
-    //   const {showTab} = this.props;
-  
       return (
         <div>
-          <button>Run Cypress Tests</button>
+          <button className='btn'>Run Cypress Tests</button>
+          <button className='btn btn--success'>Success</button>
+          <button className='btn btn--fail'>Failed</button>
         </div>
       );
     }
