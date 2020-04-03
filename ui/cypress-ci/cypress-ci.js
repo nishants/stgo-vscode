c5bfa77e2fc129c1caa90e6e7c226b72a74cdfa6
Rahul Maheshwari <rahulm@qburst.com>
2020-04-03 17:03:52 +0530

    disable start button if build in progress

--- ui/cypress-ci/cypress-ci.js
+++ ui/cypress-ci/cypress-ci.js
@@ -2,9 +2,10 @@ import React from "react";
 import Status from './status';
 
 function CypressCi ({data=[]}, callBack=()=>{}) {
+      const disabled = !![1].indexOf(data[0].status) > -1 ;
       return (
         <div className='cypressCi'>
-          <button className='btn' onClick={(e)=> callBack(e)}>Run Cypress Tests</button>
+          <button className='btn' disabled={disabled} onClick={(e)=> callBack(e)}>Run Cypress Tests</button>
           {data.map(item => <Status commitId={item.sourceVersion} result={item.result} status={item.status} commitBuildHref={item._links.web.href}/>)}
         </div>
         
