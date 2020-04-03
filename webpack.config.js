77790d2b3196c473c3e376f50141eefad409b8d0
Nishant Singh <nishant.singh87@gmail.com>
2020-04-03 08:12:08 +0530

    Add svg url loader for webpack and chevron icon (#27)
    * Add svg url loader for webpack
    
    * add chevron.

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,17 @@ module.exports = {
           // Compiles Sass to CSS
           'sass-loader',
         ],
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-url-loader',
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
       }
     ]
   },
