7c09976bbce86b2bae168146f571a3f9896fdca2
Mansi <mansi@nagarro.com>
2020-04-03 17:00:55 +0530

    design chnages in branch overview

--- ui/branch-overview/index.js
+++ ui/branch-overview/index.js
@@ -31,37 +31,49 @@ class Index extends React.Component {
     const existing = branchData.type === "EXISTING";
     var sourceRefName = existing && branchData.data.sourceRefName.split('/')[2];
     var targetRefName = existing && branchData.data.targetRefName.split('/')[2];
+    var createdByImage = branchData.data.createdBy._links.avatar.href;
+    var formattedDate = String(branchData.data.creationDate).substring(0, 10);
 
     return (
       <div className="branch-details">
         <div className='row'>
-            <span className="label">Selected Branch :</span> 
-            <span className="value">{selectedBranch}</span>
+          <span className="label">Selected Branch :</span>
+          <span className="value">{selectedBranch}</span>
         </div>
         {existing &&
           (<React.Fragment>
-              <div className='row'>
-                <span className="label">Pull Request :</span>
-                <span className="value">
-                    <span>#{branchData.data.pullRequestId}</span>
-                </span>
-              </div>
-              <div className='row'>
-                <span className="label">Title :</span>
-                <span className="value">{branchData.data.title}</span>
-              </div>
-              <div className='row'>
-                <span className="label">Description :</span>
-                <span className="value">{branchData.data.description}</span>
-              </div>
-              <div className='row'>
-                <span className="label">Source :</span>
-                <span className="value">{sourceRefName}</span>
-              </div>
-              <div className='row'> 
-                <span className="label"> Target :</span>
-                <span className="value">{targetRefName}</span>
-              </div>
+            <div className='row'>
+              <span className="label">Pull Request :</span>
+              <span className="value">
+                <span>#{branchData.data.pullRequestId}</span>
+              </span>
+            </div>
+            <div className='row'>
+              <span className="label">Title :</span>
+              <span className="value">{branchData.data.title}</span>
+            </div>
+            <div className='row'>
+              <span className="label">Description :</span>
+              <span className="value">{branchData.data.description}</span>
+            </div>
+            <div className='row'>
+              <span className="label">Created By :</span>
+              <img className="icon" src={createdByImage} /> <span className="value">
+                {branchData.data.createdBy.displayName} on {formattedDate}</span>
+            </div>
+            <div className='row'>
+              <span className="label">Reviewers :</span>
+              <img className="icon" src={branchData.data.reviewers[0].imageUrl} /> <span className="value">
+                {branchData.data.reviewers[0].displayName}</span>
+            </div>
+            <div className='row'>
+              <span className="label">Source :</span>
+              <span className="value">{sourceRefName}</span>
+            </div>
+            <div className='row'>
+              <span className="label"> Target :</span>
+              <span className="value">{targetRefName}</span>
+            </div>
           </React.Fragment>)
         }
         <div className='row no-label'>
@@ -70,9 +82,9 @@ class Index extends React.Component {
         </div>
 
         {existing ?
-          (            <div className='row no-label'>
-              <button className='button' onClick={() => this.props.openUrl(branchData.link)}>Open Pull Request</button>
-            </div>
+          (<div className='row no-label'>
+            <button className='button' onClick={() => this.props.openUrl(branchData.link)}>Open Pull Request</button>
+          </div>
           ) :
           (<div className='row no-label'>
             <button onClick={() => this.props.openUrl(branchData.link)} className='button'>Create Pull Request</button>
