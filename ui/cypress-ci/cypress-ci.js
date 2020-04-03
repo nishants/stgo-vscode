ac121caaf38730957601ee6fd65a0c5011386923
Rahul Maheshwari <rahulm@qburst.com>
2020-04-03 18:10:35 +0530

    added css

--- ui/cypress-ci/cypress-ci.js
+++ ui/cypress-ci/cypress-ci.js
@@ -2,10 +2,11 @@ import React from "react";
 import Status from './status';
 
 function CypressCi ({data=[]}, callBack=()=>{}) {
-      const disabled = !![1].indexOf(data[0].status) > -1 ;
+      const classNam = [1].indexOf(data[0].status) > -1 ? 'btn disabled' : 'btn' ;
+
       return (
         <div className='cypressCi'>
-          <button className='btn' disabled={disabled} onClick={(e)=> callBack(e)}>Run Cypress Tests</button>
+          <button className={classNam} onClick={(e)=> callBack(e)}>Run Cpress Tests</button>
           {data.map(item => <Status commitId={item.sourceVersion} result={item.result} status={item.status} commitBuildHref={item._links.web.href}/>)}
         </div>
         
