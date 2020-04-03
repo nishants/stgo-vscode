3ef46911b80c27d9d2abb18ee6ec8c6ebd310a1c
nishant <nishant.singh87@gmail.com>
2020-04-03 17:33:18 +0530

    Reload screenshot data when branch changes.

--- ui/screenshot-diffs-tab/index.js
+++ ui/screenshot-diffs-tab/index.js
@@ -2,8 +2,19 @@ import React from "react";
 import ScreenshotDiffItem from "./diff-item.js";
 
 class ScreenshotDiffs extends React.Component {
-  componentDidMount() {
+
+  loadDataForBranch(){
     this.props.getScreenshotDiffs(this.props.branchName);
+  }
+
+  componentDidMount() {
+    this.loadDataForBranch();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.currentBranchName !== this.props.currentBranchName) {
+      this.loadDataForBranch();
+    }
   }
 
   render() {
@@ -15,7 +26,12 @@ class ScreenshotDiffs extends React.Component {
             <li className='file-screenshot-diff-group'>
               <ul>
                 {
-                  diff.screenshots.map(diff => <ScreenshotDiffItem diff={diff} openUrl={openUrl} sendHttpRequest={sendHttpRequest} openFile={openFile}/>)
+                  diff.screenshots.map(diff => <ScreenshotDiffItem
+                    diff={diff}
+                    openUrl={openUrl}
+                    sendHttpRequest={sendHttpRequest}
+                    openFile={openFile}
+                  />)
                 }
               </ul>
             </li>
