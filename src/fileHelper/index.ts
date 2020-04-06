97bf8dd608b2966a977283a84edc34edfd3b57dc
hiteshdua1 <hiteshdua1@gmail.com>
2020-04-06 17:28:32 +0530

    Adding handling for direct filename and multiple screenshots in a single test case (#40)

--- src/fileHelper/index.ts
+++ src/fileHelper/index.ts
@@ -3,25 +3,47 @@ import * as vscode from "vscode";
 const cherow = require("cherow");
 const estraverse = require('estraverse');
 
-
-function findScreenshotPos(AST) {
-    let charPos;
+//TODO Cleanup this file to abstract traversing logic to a single method
+function findScreenshotName(AST, fileName) {
+    let charPos = 0;
     estraverse.traverse(AST, {
         enter: function (node, parent) {
-            if (node.type === 'Identifier' && node.name === "screenshot") {
-                charPos = node.start;
-                return estraverse.VisitorOption.Break;
+            if (node.type == 'Literal') {
+                if (node.value === fileName) {
+                    charPos = node.start;
+                    return estraverse.VisitorOption.Break;
+                }
             }
         }
     });
     return charPos;
 }
 
-function findScreenshotNode(documentAST, descriptonArr, index = 1) {
+function findScreenshotPos(AST, count) {
+    let charPos = 0;
+    estraverse.traverse(AST, {
+        enter: function (node, parent) {
+            if (node.type === 'Identifier' && node.name === "screenshot") {
+                if (count === 0) {
+                    charPos = node.start;
+                    return estraverse.VisitorOption.Break;
+                }
+                count--;
+            }
+        }
+    });
+    return charPos;
+}
 
+function findScreenshotNode(documentAST, descriptonArr, count) {
+
+    if (descriptonArr.length === 1) {
+        return findScreenshotName(documentAST, descriptonArr[0]);
+    }
+    let index = 1;
     let parentNode = documentAST;
     traverseToNode(parentNode, descriptonArr[index]);
-    return findScreenshotPos(parentNode);
+    return findScreenshotPos(parentNode, count);
 
     function traverseToNode(AST, description) {
         let foundNode = false;
@@ -36,6 +58,7 @@ function findScreenshotNode(documentAST, descriptonArr, index = 1) {
                 }
             }
         });
+
         if (foundNode && parentNode) {
             index++;
             if (index >= descriptonArr.length) {
@@ -46,6 +69,13 @@ function findScreenshotNode(documentAST, descriptonArr, index = 1) {
     }
 }
 
+function getScreenshotIndex(str) {
+    // Ref : https://stackoverflow.com/questions/432493/how-do-you-access-the-matched-groups-in-a-javascript-regular-expression
+    var myRegexp = /^.*?\([^\d]*(\d+)[^\d]*\).png$/g;
+    var match = myRegexp.exec(str);
+    return match ? match[1] : 0;
+}
+
 export default (panel: vscode.WebviewPanel, workspaceConfig: object) => {
 
     const workspaceFolder = [...vscode.workspace.workspaceFolders].pop();
@@ -53,10 +83,15 @@ export default (panel: vscode.WebviewPanel, workspaceConfig: object) => {
 
     const openFile = async ({ path, name }) => {
         const filePath = path.replace("\\drop\\cypress\\screenshots", `${workspaceFolderPath}\\src\\frontend`);
-        
-        //TODO - right now blindly removing extension name 
-        // we need to cater for count before that as well as the screenshots with name
-        name = name.replace(/\.[^/.]+$/, "");
+
+        const screenshotIndex = getScreenshotIndex(name);
+
+        // we need to cater for the screenshots with name
+        if (screenshotIndex > 0) {
+            name = name.replace(`(${screenshotIndex}).png`, '').trim()
+        } else {
+            name = name.replace(`.png`, "").trim();
+        }
 
         // array to find the screenshot in
         const descriptonArr = name.split('--').map(val => val.trim());
@@ -67,7 +102,7 @@ export default (panel: vscode.WebviewPanel, workspaceConfig: object) => {
         // create an AST of the document so that we are able to find the screenshot command
         const documentAST = cherow.parseModule(doc.getText(), { ranges: true });
 
-        const startChar = await findScreenshotNode(documentAST, descriptonArr);
+        const startChar = await findScreenshotNode(documentAST, descriptonArr, screenshotIndex);
 
         const lineNumber = doc.getText().substring(0, startChar).split('\n').length;
 
