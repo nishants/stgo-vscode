304fb633f5cf1356d753d82f844bc1e70b878530
nishant <nishant.singh87@gmail.com>
2020-04-11 22:16:43 +0530

    Refactor tfs client.

--- src/tfs/index.ts
+++ src/tfs/index.ts
@@ -1,63 +1,28 @@
-// @ts-nocheck
-import * as config from "../config";
 import * as vscode from "vscode";
-import { TFS_PROJECT, TFS_REPO, TFS_URL, getCreatePullRequestUrl} from "../constant";
-const tfs = require('azure-devops-node-api');
 
-const PR_MOCK_FILE = "pull-request-mock.json";
+import {getPullRequest} from "./tfs-connection";
+import {getMockPullRequest} from "./mock-data";
+import {getCreatePullRequestUrl} from "../constant";
 
+type WorkspaceConfig = { enableMocks: boolean, tfsToken: string };
 
-export default (panel: vscode.WebviewPanel, workspaceConfig: object) => {
-    let tfsConnection;
+export default (panel: vscode.WebviewPanel, workspaceConfig: WorkspaceConfig) => {
+    const getMockOrPullRequest = async (branchName: string) => {
 
-    const initializeTFSConnection = () => {
-        const tfsAuthHandler = tfs.getPersonalAccessTokenHandler(workspaceConfig.tfsToken);
-        tfsConnection = new tfs.WebApi(TFS_URL, tfsAuthHandler);
-    };
-
-    const getPullRequestFromTFS = async (branchName) => {
-        if(!tfsConnection){
-            initializeTFSConnection();
-        }
-
-        const gitApi = await tfsConnection.getGitApi();
-        return gitApi.getPullRequests(
-            TFS_REPO,
-            {"sourceRefName": `refs/heads/${branchName}`},
-            TFS_PROJECT,
-            10,
-            0,
-            10);
-    };
-
-    const getPullRequest = async ({branchName}: object) => {
-        if (workspaceConfig.enableMocks) {
-            return config.getData(PR_MOCK_FILE).then(mockPullRequest => {
-                vscode.window.showInformationMessage(
-                    `Returning mock data for ${branchName}`
-                );
-                panel.webview.postMessage({
-                    messageId: "set-pull-request",
-                    data:{
-                        pullRequest: mockPullRequest,
-                        createPullRequestUrl: getCreatePullRequestUrl(branchName)
-                    }
-                });
-            });
-        }
-
-        const prDetails = await getPullRequestFromTFS(branchName);
+        const pullRequests = workspaceConfig.enableMocks
+            ? await getMockPullRequest(branchName)
+            : await getPullRequest(branchName, workspaceConfig.tfsToken);
 
         panel.webview.postMessage({
             messageId: "set-pull-request",
             data: {
-                pullRequest: prDetails[0],
+                pullRequest: pullRequests[0],
                 createPullRequestUrl: getCreatePullRequestUrl(branchName)
             }
         });
     };
 
     return {
-        getPullRequest
+        getPullRequest: getMockOrPullRequest
     };
 };
---
+++ src/tfs/tfs-connection.ts
@@ -0,0 +1,22 @@
+import {TFS_PROJECT, TFS_REPO, TFS_URL} from "../constant";
+const tfs = require('azure-devops-node-api');
+
+let tfsConnection: any;
+
+const createConnection = (tfsToken: string) => {
+    const tfsAuthHandler = tfs.getPersonalAccessTokenHandler(tfsToken);
+    tfsConnection = new tfs.WebApi(TFS_URL, tfsAuthHandler);
+    return tfsConnection;
+};
+
+export const getPullRequest = async (branchName: string, tfsToken: string) => {
+    const gitApi = await (tfsConnection || createConnection(tfsToken)).getGitApi();
+
+    return gitApi.getPullRequests(
+        TFS_REPO,
+        {"sourceRefName": `refs/heads/${branchName}`},
+        TFS_PROJECT,
+        10,
+        0,
+        10);
+};
