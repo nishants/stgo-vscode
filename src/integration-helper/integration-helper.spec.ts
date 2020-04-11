3cdeb28205e244d05d7395fb462fbc70deb1b7b1
nishant <nishant.singh87@gmail.com>
2020-04-12 03:14:12 +0530

    move mocks to src.

--- src/integration-helper/integration-helper.spec.ts
+++ src/integration-helper/integration-helper.spec.ts
@@ -1,27 +1,24 @@
-import * as vscode from "vscode";
-
 jest.mock('vscode');
 jest.mock('../utils');
 
+import * as vscode from "vscode";
 import * as utils from "../utils";
+import panel from "../__mocks__/fakePanel";
 
 import handler from "./index";
 
 describe('src/integration-helper', () => {
     test('should fetch screenshot diffs for branches', async () => {
-        const panel = {
-            webview: {postMessage: jest.fn()}
-        };
         const workspaceConfig = {};
         // @ts-ignore
         utils.getJsonOverHttp = () => new Promise((resolve) => {
             resolve([{branchName: 'dabba'}])
         })
 
-        // const diffs = await helper(null, null);
         console.log({vscode});
-        // @ts-ignore
+
         await handler(panel, workspaceConfig).getAllBranches();
+        // @ts-ignore
         expect(panel.webview.postMessage.mock.calls).toEqual([[
             {"data": [{"name": "dabba"}], "messageId": "set-branch-list"}
         ]]);
