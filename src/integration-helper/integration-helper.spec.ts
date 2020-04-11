eede8c39a2a139027b21d6ed368f260a4674f4be
nishant <nishant.singh87@gmail.com>
2020-04-12 02:52:08 +0530

    setup a test with mocks.

--- src/integration-helper/integration-helper.spec.ts
+++ src/integration-helper/integration-helper.spec.ts
@@ -1,19 +1,30 @@
 import * as vscode from "vscode";
 
 jest.mock('vscode');
-import chaloBeta from "./index";
+jest.mock('../utils');
 
-// const ss = require("../../__mocks__/vscode");
+import * as utils from "../utils";
+
+import handler from "./index";
 
 describe('src/integration-helper', () => {
     test('should fetch screenshot diffs for branches', async () => {
-        const panel = {};
+        const panel = {
+            webview: {postMessage: jest.fn()}
+        };
         const workspaceConfig = {};
+        // @ts-ignore
+        utils.getJsonOverHttp = () => new Promise((resolve) => {
+            resolve([{branchName: 'dabba'}])
+        })
 
         // const diffs = await helper(null, null);
         console.log({vscode});
         // @ts-ignore
-        chaloBeta(panel, workspaceConfig);
-        expect(vscode.window.activeTextEditor).toEqual('dosa');
+        await handler(panel, workspaceConfig).getAllBranches();
+        expect(panel.webview.postMessage.mock.calls).toEqual([[
+            {"data": [{"name": "dabba"}], "messageId": "set-branch-list"}
+        ]]);
+
     });
 });
