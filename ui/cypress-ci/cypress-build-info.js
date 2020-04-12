7a0fd421f0d5c00e150d19a88aee0a9378a5eff5
nishant <nishant.singh87@gmail.com>
2020-04-12 17:02:45 +0530

    refactor cypress build info.

--- ui/cypress-ci/cypress-build-info.js
+++ ui/cypress-ci/cypress-build-info.js
@@ -1,17 +1,22 @@
 import React from "react";
 
-function CypressBuildInfo({ commitId, result, status , commitBuildHref, buildTime}) {
+function CypressBuildInfo({commitId, result, commitBuildHref, buildTime}) {
   const success = [2, 4].indexOf(result) !== -1 ? true : false;
   const failure = [8].indexOf(result) !== -1 ? true : false;
   const cancelled = [32].indexOf(result) !== -1 ? true : false;
+  const statusClass = success ? 'success' : failure ? 'error' : 'warning';
+  const label = success ? 'Success' : failure ? 'Failed' : cancelled ? 'Cancelled' : 'Unknown';
+
   return (
     <div className="build-status" key={commitId}>
       Commit
       <div>{commitId}</div>
       <div>{buildTime}</div>
-      {success && <span className="status-flag success"><a href={commitBuildHref}>Success</a></span>}
-      {failure && <span className="status-flag error"><a href={commitBuildHref}>Failed</a></span>}
-      {cancelled && <span className="status-flag warning"><a href={commitBuildHref}>Cancelled</a></span>}
+      <span
+        className={`status-flag ${statusClass}`}>
+        <a
+          href={commitBuildHref}>{label}</a>
+      </span>
     </div>
   );
 }
